ce2b1460903f6a17857d61e01f1b5c8b5f3098a5
BryanB-Dev <perso703@pm.me>
2023-11-13 16:59:24 +0100

    Rate

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import Header from "./components/organisms/Header";
 import Banner from "./components/molecules/Banner";
-import BannerImg from "./assets/images/banner-1.png";
 import Host from "./components/molecules/Host";
+import BannerImg from "./assets/images/banner-1.png";
 import Footer from "./components/organisms/Footer";
-import "./styles/style.scss";
-import Card from "./components/molecules/Card";
 import CardContainer from "./components/organisms/CardContainer";
+import "./styles/style.scss";
+import Rate from "./components/molecules/Rate";
+
+const test = 5;
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner src={BannerImg} title="Chez vous, partout et ailleurs"/>
-      {/* <Host /> */}
       <CardContainer />
+      <Host />
+      <Rate rating={test}/>
       <Footer />
     </div>
   );

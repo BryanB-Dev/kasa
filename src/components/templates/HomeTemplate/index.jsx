91e0cc7f86aa928a35f67da535c9e0e44b006c32
BryanB-Dev <perso703@pm.me>
2023-11-15 17:48:57 +0100

    Home Page > Banner

--- src/components/templates/HomeTemplate/index.jsx
+++ src/components/templates/HomeTemplate/index.jsx
@@ -1,6 +1,8 @@
 import Header from "../../organisms/Header"
 import Footer from "../../organisms/Footer"
 import CardContainer from "../../organisms/CardContainer";
+import Banner from "../../molecules/Banner";
+import BannerImg from "../../../assets/images/banner-1.png";
 
 export default function HomeTemplate({ datas }) {
 
@@ -9,6 +11,7 @@ export default function HomeTemplate({ datas }) {
     return (
         <>
             <Header location={location} />
+            <Banner src={BannerImg} title="Chez vous, partout et ailleurs"/>
             <CardContainer datas={datas} />
             <Footer />
         </>

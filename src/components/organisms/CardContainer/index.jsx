ab89ec14ba4cded6b7cd5dd18c825cd83de8438c
BryanB-Dev <perso703@pm.me>
2023-11-28 16:19:34 +0100

    Fix Key

--- src/components/organisms/CardContainer/index.jsx
+++ src/components/organisms/CardContainer/index.jsx
@@ -4,8 +4,8 @@ import "./style.scss";
 export default function CardContainer({ datas }) {
   return (
     <section className="cardContainer">
-      {datas.map(({ id, title, cover }) => (
-        <Card img={cover} title={title} href={`/infos/${id}`} />
+      {datas.map(({ id, title, cover }, index) => (
+        <Card img={cover} title={title} href={`/infos/${id}`} key={index}/>
       ))}
     </section>
   );
--- src/components/organisms/CollapseContainer/index.jsx
+++ src/components/organisms/CollapseContainer/index.jsx
@@ -2,11 +2,10 @@ import Collapse from "../../atoms/Collapse";
 import "./style.scss";
 
 export default function CollapseContainer({ datas, row }) {
-    console.log(datas)
   return (
     <section className={"collapseContainer " + (row ? 'collapseContainer--row' : 'collapseContainer--column')}>
-      {datas.map(({ title, content }) => (
-        <Collapse title={title} content={content} />
+      {datas.map(({ title, content }, index) => (
+        <Collapse title={title} content={content} key={index} />
       ))}
     </section>
   );
